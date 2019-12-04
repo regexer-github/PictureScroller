0c0e915e8708e8ce49c6f51d658ef7f88e441b87
Michiel <michiel.vdbos@gmail.com>
2019-12-04 22:52:05 +0100

    howler

--- src/app/containers/category-detail/category-detail.component.ts
+++ src/app/containers/category-detail/category-detail.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { pluck, take } from 'rxjs/operators';
-
+import { Howl } from 'howler';
 @Component({
     selector: 'app-category-detail',
     templateUrl: './category-detail.component.html',
@@ -11,7 +11,6 @@ import { pluck, take } from 'rxjs/operators';
 export class CategoryDetailComponent implements OnInit {
     imagePaths: string[];
     animal$: Observable<string>;
-
     constructor(private route: ActivatedRoute, private router: Router) {}
 
     ngOnInit() {
@@ -27,9 +26,9 @@ export class CategoryDetailComponent implements OnInit {
         this.animal$.subscribe(animal => this.playAnimalSound(animal));
     }
     playAnimalSound(animal: string) {
-        const audio = new Audio();
-        audio.src = `../../../assets/audio/${animal}.wav`;
-        audio.load();
+        const audio = new Howl({ src: [`../../../assets/audio/${animal}.wav`] });
+        // audio.src = `../../../assets/audio/${animal}.wav`;
+        // audio.load();
         audio.play();
     }
 }
